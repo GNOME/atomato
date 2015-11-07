7803cc448f47205bf9e2305791796916ea5bab18
Rodrigo Moya <rodrigo@gnome-db.org>
2015-11-07 09:48:31 +0100

    Switch to Vala and modernize build system

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,7 @@ test -z "$srcdir" && srcdir=.
 PKG_NAME="atomato"
 REQUIRED_AUTOMAKE_VERSION=1.9
 
-if [ ! -f "$srcdir/libatomato/atomato.c" ]; then
+if [ ! -f "$srcdir/src/main.vala" ]; then
 	echo "$srcdir doesn't look like source directory for $PKG_NAME" >&2
 	exit 1
 fi
